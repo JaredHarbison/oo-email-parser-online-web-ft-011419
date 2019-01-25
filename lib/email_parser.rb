bb02b7c4000842b2848c3a9945a5c1c5a6c5bae1
Jared Harbison <jared.harbison@gmail.com>
2019-01-25 21:36:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,4 +9,11 @@ class Email_parser
     @@email = emails 
   end 
   
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
+  
 end 
